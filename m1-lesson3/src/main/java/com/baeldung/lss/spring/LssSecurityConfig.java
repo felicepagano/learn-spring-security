c501fda84d5f237ef043364e5341fbd6786090a6
Felice Pagano <paganofelice@gmail.com>
2018-09-07 16:49:13 +0200

    override authorization parameter
    override configure method in order to customize URL authorizaton

--- m1-lesson3/src/main/java/com/baeldung/lss/spring/LssSecurityConfig.java
+++ m1-lesson3/src/main/java/com/baeldung/lss/spring/LssSecurityConfig.java
@@ -2,24 +2,35 @@ package com.baeldung.lss.spring;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
 public class LssSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    public LssSecurityConfig() {
-        super();
-    }
-
-    //
-
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception { // @formatter:off 
         auth.
             inMemoryAuthentication().
-            withUser("user").password("pass").
-            roles("USER");
+            withUser("user").password("pass").roles("USER");
     } // @formatter:on
 
+	@Override
+	protected void configure(HttpSecurity http) throws Exception { // @formatter:off
+		http
+		// authorization of the URL 
+		// every requests need to be authenticated
+		.authorizeRequests()
+			.anyRequest().authenticated()
+			// with hasRole API your principle must have the ROLE_ADMIN role. this because spring automatically prefixed with ROLE_.
+			// To avoid prefix you must use hasAutority API .antMatchers("/delete/**").hasAutority("ADMIN"). with any you will be able to use more than one role.
+			.antMatchers("/delete/**").hasAuthority("ADMIN")
+			// .antMatchers("/delete/**").hasAnyAuthority("ADMIN", "ADMIN2")
+			// .antMatchers("/admin").hasIpAddress(ipaddressExpression)
+			.and()
+		.formLogin().and()
+		.httpBasic();
+	} // @formatter:on 
+    
 }
